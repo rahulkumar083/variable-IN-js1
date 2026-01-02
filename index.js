b96235c987e58b2d556a478537a436fe41472027
rahulkumar083 <136946107+rahulkumar083@users.noreply.github.com>
2026-01-02 11:20:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+ let a1 = 10;
+ let a2 = "rahul";
+ console.log(a1);
+ console.log(a2);
+ a=9;
+ console.log(a1);
+ //When you declare a variable using let, you cannot declare it again with the same name in the same block.
+// no redcalre only re assign the vale
+ const b1=11
+ const b2="kumar"
+ console.log(b1);
+console.log(b2);
+ //b1=12
+//const is used to declare a constant variable — a variable whose value cannot be changed or redeclared.
+//no redcalre no assign the vale
+console.log(b1);
+console.log(b2);
+
+var c1=13;
+var c2="verma";
+//redcalre re assign  both are possble
+
+console.log(c1);
+console.log(c2);
+c1=083
+console.log(c1);
+
